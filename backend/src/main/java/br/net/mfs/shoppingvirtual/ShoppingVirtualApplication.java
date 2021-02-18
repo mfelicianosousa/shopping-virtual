458658c76da30153555c72b03fb771744ec14b87
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-02-18 00:29:37 -0400

    Project Shopping-virtual created

---
+++ backend/src/main/java/br/net/mfs/shoppingvirtual/ShoppingVirtualApplication.java
@@ -0,0 +1,13 @@
+package br.net.mfs.shoppingvirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingVirtualApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/net/mfs/shoppingvirtual/ShoppingVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.mfs.shoppingvirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
